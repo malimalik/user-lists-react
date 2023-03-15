92f270c2bc5d791188c9f32743b57c76b05f1f5a
Ali Malik <ialimalik99@gmail.com>
2023-03-15 12:40:35 -0400

    Replace divs with wrapper components

---
+++ src/components/UI/ErrorModal.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import Card from "./Card";
+import Button from "./Button";
+import Wrapper from "../Helpers/Wrapper";
+import classes from "../UI/ErrorModal.module.css";
+
+const ErrorModal = (props) => {
+  return (
+    <Wrapper>
+      <div className={classes.backdrop} onClick={props.onConfirm} />
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+          <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+      </Card>
+    </Wrapper>
+  );
+};
+
+export default ErrorModal;
